fc289e8f49b0db246043ff2989888fb83cfc36c9
Taha Firoz <i170323@nu.edu.pk>
2022-06-22 22:41:56 +0500

    writing libmeterfeeder wrapper

---
+++ libmeterfeeder-rs/build.rs
@@ -0,0 +1,6 @@
+fn main() {
+    // tell rustc to link with some libhello.a library
+    println!("cargo:rustc-link=:libmeterfeeder.so");
+    // and it should search the Cargo.toml directory for that library
+    println!("cargo:rustc-link-search={}", std::env::var("CARGO_MANIFEST_DIR").unwrap());
+}
